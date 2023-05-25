86b878fdea87127ad8c531a840a4e0efbd678b22
Veronica Johnson <64655526+VKS-8@users.noreply.github.com>
2023-05-24 22:01:29 -0500

    Update code related to principal, rate, years

--- main.js
+++ main.js
@@ -1,61 +1,90 @@
 let log = console.log;
 
 const getForm = document.querySelector('#form');
-    log('GETform ' + getForm);
+  log('GETform ' + getForm);
 let result = document.querySelector('#result');
-    log('GETresult ' + result);
+  log('GETresult ' + result);
+  
 let updatedPrincipal = '';
+  log('SETupdatedPrincipal to empty ' + updatedPrincipal);
 let updatedRate = '';
+  log('SETupdatedRate to empty ' + updatedRate);
 let updatedYear = '';
+  log('SETupdatedYear to empty ' + updatedYear);
 
 // When outside the main(), it will display the result but with interest
 // undefined.
 // When inside the main(), it console logs correct interest value but
 // won't display the result text
-// getForm.addEventListener('submit', onSubmit);
-//     log('Add event listener to form onSubmit()');
+getForm.addEventListener('submit', onSubmit);
+    log('Add event listener to form onSubmit()');
 
+// Get principal from HTML
 let principal = document.querySelector('#principal');
-principal.addEventListener('input', ()=>{
-    log('principal event entry: ' + principal.value);
-  updatedPrincipal = principal.value;
-    log('updatedPrincipal: ' + updatedPrincipal);
-})
+  log('get principal from HTML ' + principal);
 
+// Get updated principal value and assign to new variable
+function getPrincipal() {
+  principal.addEventListener('input', ()=>{
+    log('principal event entry: ' + principal.value);
+    updatedPrincipal = principal.value;
+    return updatedPrincipal;
+  })
+}
+
+// Get rate_val from HTML
 let rate_val = document.querySelector('#rate_val');
-rate_val.addEventListener('input', ()=>{
-    log('rate event entry: ' + rate_val.value);
-  updatedRate = rate_val.value;
-    log('updatedRate: ' + updatedRate);
-})
-  
+log('get rate_val from HTML' + rate_val);
+
+// Get updated rate value and assign to new variable
+function getRate() {
+  rate_val.addEventListener('input', ()=>{
+      log('rate event entry: ' + rate_val.value);
+      updatedRate = rate_val.value;
+      return updatedRate;
+  })
+}
+
+// Get years from HTML
 let years = document.querySelector('#years');
-  log(years);
+  log('get years from HTML' + years);
+
+// Get updated years value and assign to new variable
+function getYear() {
   years.addEventListener('input', ()=>{
     log('years event entry: ' + years.value);
-  updatedYear = years.value;
-    log('updatedYear: ' + updatedYear);
-})
-
-let interest = document.querySelector('#interest').valueAsNumber;
+    updatedYear = years.value;
+    return updatedYear;
+  })
+}
 
 function main() {
-        log('main() entry');
-    getForm.addEventListener('submit', onSubmit);
-      log('Add event listener to form onSubmit()');
-    calcInterest();
-      log('main() calcInterest');
+      log('main() entry');
+    // getForm.addEventListener('submit', onSubmit);
+    //   log('Add event listener to form onSubmit()');
     onSubmit();
-        log('main() onSubmit()');
+      log('main() onSubmit()');
+    calcInterest();
+      log('main() calcInterest()');
+    setResult();
+      log('main() setResult()')
+    
   
 }
 
 // Computes the interest
-function calcInterest(updatedPrincipal, updatedRate, updatedYear) {
-    log('calcInterest() entry')   
+function calcInterest() {
+    log('calcInterest() entry') 
+  getPrincipal(); 
+    log('calcInterest(): getPrincipal complete');
+  getRate();
+    log('calcRate(): getRate complete');
+  getYear();
+    log('calcInterest(): getYear complete');
     log(`calcInterest(): principal: ${updatedPrincipal}, rate: ${updatedRate}, years: ${updatedYear}`)
   interest = updatedPrincipal * updatedYear * updatedRate /100;
     log('calcInterest ' + interest);
+    return interest;
 }
 
 // Gets new value from the range slider and updates the number in the #rate_val span,
@@ -63,9 +92,9 @@ function calcInterest(updatedPrincipal, updatedRate, updatedYear) {
 function updateRate(_rate_val) {
     log('updateRate() entry');
   let rateVal = document.getElementById('rate').value;
-    log('rateVal ' + rateVal);
+    log('GETrateVal ' + rateVal);
   document.getElementById('rate_val').innerHTML = `${rateVal}%`;
-    log('rate_val.innerHTML');
+    log('SETrate_val to ' + rate_val.innerHTML);
 }
 
 function onSubmit(principal) {
@@ -79,50 +108,38 @@ function onSubmit(principal) {
         log("onSubmit() preventDefault");
     alert("Please enter an amount.");
         log('alert text');
-  } else {
-    result.innerHTML = resultStr();
-      log('result.innerHTML')
-    setTimeout(() => result.innerHTML = '', 5000);
-      log('setTimeout');
-  }
+  } else return;
 }
 
-function resultStr(principal, rate_val, years, interest) {
-      log("RSprincipal " + principal);
-      log("RSinterest rate " + rate_val);
+function resultStr() {
+  getPrincipal();
+  getRate();
+  getYear();
+  calcInterest();
+    log('resultStr() entry');
+      log("RSprincipal " + updatedPrincipal);
+      log("RSinterest rate " + updatedRate);
       log("RSinterest " + interest);
-      log("RSyears " + years);
+      log("RSyears " + updatedYear);
   return(
-      `If you deposit $<span class="resultStr"><mark>${principal}</mark></span> \<br\>
-      at an interest rate of <span class="resultStr"><mark>${rate_val}%</mark></span>, \<br\>
+      `If you deposit <span class="resultStr"><mark>$${updatedPrincipal}</mark></span> \<br\>
+      at an interest rate of <span class="resultStr"><mark>${updatedRate}%</mark></span>, \<br\>
       you will receive an amount of $<span class="resultStr"><mark>${interest}</mark></span> \<br\>
-      in the year <span class="resultStr"><mark>${years}</mark></span>. \<b\>`
+      in the year <span class="resultStr"><mark>${updatedYear}</mark></span>. \<b\>`
       );
        
+}
+
+function setResult() {
+  result.innerHTML = resultStr();
+  log('result.innerHTML')
+setTimeout(() => result.innerHTML = '', 5000);
+  log('setTimeout');
 }
 
 main();
 // getPrincipal.addEventListener('blur', check);
 
-
 // const getMessage = document.querySelector('.msg');
-
-
-
-
-
-
-//     document.querySelector('result').innerHTML= resultStr;
-   
-//     // Listens for the 'computer interest' button to be clicked then runs the compute function
-//     }
-
-
-//     principal.addEventListener('change', checkCompletion, false);
-//     rate_val.addEventListener('change', checkCompletion, false);
-//     principal.addEventListener('change', checkCompletion, false);
-//     principal.addEventListener('change', checkCompletion, false);
-// }
-
 
 // let year = new Date().getFullYear()+parseInt(years);
